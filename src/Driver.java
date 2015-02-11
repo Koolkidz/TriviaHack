489cc352b66b75918622447e6e70655cff224b37
Kenny Jiang <sirkuanjiang@gmail.com>
2015-02-10 19:59:51 -0500

    As of FEB/10 7:59PM
    test class roboto

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,40 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("vroom vroom");
 
+		try {
+
+			Robot robot = new Robot();
+			//open browser (chrome 4 lyfe)
+			//run pulogulamu
+			//clic address bar
+			//???
+
+			robot.delay(4000);
+			robot.keyPress(KeyEvent.VK_F);
+			robot.keyPress(KeyEvent.VK_A);
+			robot.keyPress(KeyEvent.VK_C);
+			robot.keyPress(KeyEvent.VK_E);
+			robot.keyPress(KeyEvent.VK_B);
+			robot.keyPress(KeyEvent.VK_O);
+			robot.keyPress(KeyEvent.VK_O);
+			robot.keyPress(KeyEvent.VK_K);
+			robot.keyPress(KeyEvent.VK_PERIOD);
+			robot.keyPress(KeyEvent.VK_C);
+			robot.keyPress(KeyEvent.VK_O);
+			robot.keyPress(KeyEvent.VK_M);
+			robot.keyPress(KeyEvent.VK_ENTER);
+
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
